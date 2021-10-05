57032c6749eb2b0229054f6cb3831c2dfd3d28a4
nutchanonc <nutchanonc@github.com>
2021-10-05 18:17:51 +0700

    edit token1 & token2 for faucet

--- contracts/Token2.sol
+++ contracts/Token2.sol
@@ -5,6 +5,9 @@ import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 contract Token2 is ERC20Detailed, ERC20 {
     constructor() ERC20Detailed('Token2', 'TK2', 18) public {
-        _mint(msg.sender, 100000);
+        _mint(msg.sender, 1000000000);
+    }
+    function faucet() public returns (bool){
+        _mint(msg.sender, 1000000000);
     }
 }
